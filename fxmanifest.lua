534407d7373a4dc5e291681dfd938916edc2ef0d
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2025-08-06 05:52:13 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mega Utilities\' Mega Group'
+description 'This resource checks for updates for the callout fivepd-json'
+version '1.0.0'
+
+server_script updater.lua
