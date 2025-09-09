19ee0b309b42c200e962031aa370550a28bd067a
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2025-09-09 19:14:59 -0400

    bump version to test update checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Mega Utilities\' Mega Group'
 description 'This resource checks for updates for the callout fivepd-json'
-version '1.0.0'
+version '1.0.1'
 
 
 server_script 'updater.lua'
@@ -12,3 +12,4 @@ changelog [[
 - Added update checker for callouts
 - Added changelog support in updater
 ]]
+
