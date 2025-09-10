1879929736f28f0b81dab2b9dc2fb070648d225b
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2025-09-10 00:36:25 -0400

    Add project files.

---
+++ Exports.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using static CitizenFX.Core.Native.API;
+using FivePD.API;
+using Newtonsoft.Json;
+
+namespace fivepd.json_event_exports
+{
+    class exports : BaseScript
+    {
+        bool OnDuty = false;
+
+        public exports()
+        {
+            EventHandlers["fivepd_json_exports:OnDuty"] += new Action<bool>(OnDutyChanged);
+            EventHandlers["fivepd_json_exports:OffDuty"] += new Action(OffDuty);
+
+            Events.OnDutyStatusChange += OnDutyChange;
+        }
+        
+        private void OnDutyChanged(bool onDuty)
+        {
+            OnDuty = onDuty;
+            Events.InvokeDutyEvent(onDuty);
+        }
+    }
+}
---
+++ JsonPluginBridge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fivepd_json_plugin
+{
+    public class JsonPluginBridge
+    {
+        // This is coming soon and will allow you to make Plugins using JSON 
+    }
+}
