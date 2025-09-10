51f01d2e634424351686519622ffde3de2cb07a8
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2025-09-09 21:04:42 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,13 @@ game 'gta5'
 
 author 'Mega Utilities\' Mega Group'
 description 'This resource checks for updates for the callout fivepd-json'
-version '1.0.1'
+version '1.0.0'
 
 
 server_script 'updater.lua'
 
 changelog [[
-- Added update checker for callouts
 - Added changelog support in updater
 ]]
+
 
